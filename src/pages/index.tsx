25776c9afe2cc2d0f050233ff373bfa6173e17a0
thun2maru <107395677+thun2maru@users.noreply.github.com>
2024-06-11 02:07:17 +0900

    初期候補地

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,31 +6,61 @@ const Home = () => {
   const [board, setboard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 2, 0, 0, 0],
-    [0, 0, 0, 2, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 3, 0, 0, 0],
+    [0, 0, 0, 1, 2, 3, 0, 0],
+    [0, 0, 3, 2, 1, 0, 0, 0],
+    [0, 0, 0, 3, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+
+  const directions = [
+    [0, 1], // 下
+    [0, -1], // 上
+    [1, 1], // 右下
+    [1, -1], // 右上
+    [1, 0], // 右
+    [-1, 0], // 左
+    [-1, 1], // 左下
+    [-1, -1], // 左上
+  ];
+
+  //   候補地=
+  // 全マス確認board[0,0,]-[7,7]
+  // 候補地=[x,y]
+  // [x+1,0]until x<8
+  // if x<8
+  // x=1
+  // if 置けるマス（定義されていて石が置かれていないかつその方向に自分の石がある）
+  // の時候補地
+  // const choice = (x, y) => {
+  //   const around = [];
+  //   for (const direction of directions) {
+  //     const [dx, dy] = direction;
+  //     const preX = x + dx;
+  //     const preY = y + dy;
+  //     if (board[preY] !== undefined && board[preY][preX] !== undefined) {
+  //       around.push([preX, preY]);
+  //     }
+  //     return around;
+  //   }
+  // };
+
+  // const allAround = {};
+  // const y = 0;
+  // while (y < 8) {
+  //   const x = 0;
+  //   while (x < y) {
+  //     allAround[`${x},{y}`] = choice(board, x, y);
+  //   }
+  // }
+
   const clickHandler = (x: number, y: number) => {
-    if (board[y][x] !== 0) return;
+    if (board[y][x] !== 0 && board[y][x] !== 3) return;
     console.log(x, y);
     const newBoard = structuredClone(board);
 
-    const directions = [
-      [0, 1], // 下
-      [0, -1], // 上
-      [1, 1], // 右下
-      [1, -1], // 右上
-      [1, 0], // 右
-      [-1, 0], // 左
-      [-1, 1], // 左下
-      [-1, -1], // 左上
-    ];
-
     //８方向を確認してひっくり返す
-
     for (const direction of directions) {
       const [dx, dy] = direction;
       let X = x + dx;
@@ -62,10 +92,10 @@ const Home = () => {
     setboard(newBoard);
   };
 
-  const countStones = (board) => {
+  // 各石の数を数える
+  const countStones = () => {
     let black = 0;
     let white = 0;
-
     for (const row of board) {
       for (const cell of row) {
         if (cell === 1) black++;
@@ -74,7 +104,7 @@ const Home = () => {
     }
     return { black, white };
   };
-  const { black, white } = countStones(board);
+  const { black, white } = countStones();
 
   return (
     <div className={styles.container}>
@@ -82,16 +112,23 @@ const Home = () => {
         {board.map((row, y) =>
           row.map((color, x) => (
             <div className={styles.cellStyle} key={`${x}-${y}`} onClick={() => clickHandler(x, y)}>
-              {color !== 0 && (
+              {color !== 0 && color !== 3 && (
                 <div
                   className={styles.stoneStyle}
                   style={{ background: color === 1 ? '#000' : '#fff' }}
+                />
+              )}
+              {color === 3 && (
+                <div
+                  className={styles.stoneStyle}
+                  style={{ background: color === 3 ? '#ff0000' : '' }}
                 />
               )}
             </div>
           )),
         )}
       </div>
+
       <div className={styles.scoreBoard}>
         <p>黒:{black}</p>
         <p>白:{white}</p>
